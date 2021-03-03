f9f079946e3e6776b91c67d6bc0795c8025c0c59
sora-coder <rqqgx271@yahoo.co.jp>
2021-03-03 17:14:02 +0900

    4-9.php

---
+++ 4-9.php
@@ -0,0 +1,9 @@
+<?php
+// require('4-9_include.php');
+// require'4-9_include.php';
+// require '4-9_include.php';
+
+// require_once '4-9_include.php';    //１回呼び出されていたらもう呼び出されない
+include_once '4-9_include.php';
+
+echo sum(10, 15);
