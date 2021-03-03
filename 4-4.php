50b532d7d11acfe53aac8e298a7237ce9c169cd8
sora-coder <rqqgx271@yahoo.co.jp>
2021-03-03 09:47:36 +0900

    4-4.php

---
+++ 4-4.php
@@ -0,0 +1,15 @@
+<?php
+//リストを作る
+function make_list($list, $head = '・'){
+    foreach($list as $key => $val){
+        echo $head, $key, ' : ', $val, "\n";
+    }
+}
+$pref = [
+    'hokkaido' => '北海道',
+    'aomori' => '青森県',
+    'iwate' => '岩手県'
+];
+
+make_list($pref);
+make_list($pref, '>');
