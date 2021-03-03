d9599ad10b5fd2ca7f40aaf4ba6c4454fc952236
sora-coder <rqqgx271@yahoo.co.jp>
2021-03-03 23:01:27 +0900

    5-1.php オブジェクト指向

--- 5-1.php
+++ 5-1.php
@@ -1,5 +1,10 @@
 <?php
-date_default_timezone_set('Asia/Tokyo');    //タイムゾーンを変更する
-$today = time();
-$format = date('Y/m/d', $today);
-echo $format;
+//functionベース
+// date_default_timezone_set('Asia/Tokyo');    //タイムゾーンを変更する
+// $today = time();
+// $format = date('Y/m/d', $today);
+// echo $format;
+
+$today = new DateTime();         //DateTimeのオブジェクトを作る
+$today->setTimezone(new DateTimeZone('Asia/Tokyo'));
+echo $today->format('Y/m/d');
