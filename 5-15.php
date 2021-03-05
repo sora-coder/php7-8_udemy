35bd181f6ec4e98ee79f8250837dac56c711fd68
sora-coder <rqqgx271@yahoo.co.jp>
2021-03-05 12:21:23 +0900

    5-15.php

--- 5-15.php
+++ 5-15.php
@@ -1,11 +1,15 @@
 <?php
 class Item {
-    public int $tax = 10;//消費税
+    public static int $tax = 10;//staticを記述
 
-    public function getTax(): int{
-        return $this->tax;
+    public static function getTax(): int{//staticを記述
+        return self::$tax;     //staticのプロパティを呼び出す時は$thisではなくself::を使う
     }
 }
+echo Item::getTax();           //インスタンスを作らなくてもメソッドを呼び出せる
 
-$item = new Item();
-echo $item->getTax();
+echo Item::$tax, "\n";     //publicなら呼び出せる
+Item::$tax = 8;      //プロパティの変更ができる(クラスのプロパティ自体が変更になる)
+
+$item02 = new Item();
+echo $item02->getTax();    // 変更後の8が表示される
